76fd57977e7e535e37602aa8148f0e537845cc43
sljmn <sulejmanhot@gmail.com>
2020-09-30 20:31:08 +0200

    retry with cors

--- config/application.rb
+++ config/application.rb
@@ -23,13 +23,7 @@ module SmartSpotterAssignment
     config.load_defaults 5.2
     config.time_zone = 'Europe/Amsterdam'
 
-    config.middleware.insert_before 0, Rack::Cors do
-        allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-        end
-    end
-
+    s
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
